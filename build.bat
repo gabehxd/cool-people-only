22cfe7797498050adb26d1138adfe124c540cb08
gabeh <gabehttps@gmail.com>
2022-07-25 22:50:06 -0700

    second pass (works)

--- build.bat
+++ build.bat
@@ -1,7 +1,13 @@
 @echo off
-xcopy %1 "Fabulously Optimized\"
-cd "Fabulously Optimized"
+mkdir fo\
+xcopy %1 "fo\"
+cd "fo"
 packwiz.exe curseforge import %1
 cd ..
-xcopy "Cool People Only!\" "output\"
-xcopy s:\ z:\ /S /Y /D "Fabulously Optimized\" "output\"
+xcopy "fo\" "output\" /S /Y
+xcopy "Cool People Only!\" "output\" /S /Y
+cd "output\"
+del %1
+packwiz.exe refresh
+packwiz.exe update --all
+echo Make sure to update isxander-main-menu-credits.json!
