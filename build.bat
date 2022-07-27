5a1754cc28c4c82d9627fd755739b8a7fe50867e
gabeh <gabehttps@gmail.com>
2022-07-27 15:55:52 -0700

    update index on build
    The one exception im going to make for changing files during build time for ECPO

--- build.bat
+++ build.bat
@@ -3,14 +3,19 @@ mkdir fo\
 mkdir output\
 xcopy %1 "fo\"
 cd "fo"
-packwiz.exe curseforge import %1
+packwiz curseforge import %1
 del %1
 packwiz update --all
 cd ..
 xcopy "fo\" "output\" /S /Y
+cd "Enhancements for Cool People Only!"
+packwiz refresh
+echo Project files have been refreshed for you! Make sure to commit if index files have updated!
+set /p DUMMY=Hit ENTER to continue...
+cd ..
 xcopy "Enhancements for Cool People Only!\" "output\" /S /Y
 cd "output\"
-packwiz.exe refresh
+packwiz refresh
 echo Make sure to update isxander-main-menu-credits.json!
 set /p DUMMY=Hit ENTER to continue...
-packwiz.exe curseforge export
+packwiz curseforge export
